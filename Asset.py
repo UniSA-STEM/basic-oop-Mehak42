04f09d3d71f06844c69e1fef471bfa15ce6644da
Mehak42 <tiwmy001@mymail.unisa.edu.au>
2025-10-11 17:09:40 +1030

    Used property attribute and string representation of the asset

--- Asset.py
+++ Asset.py
@@ -7,27 +7,41 @@ Username: TIWMY001
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
 
-"""This class represents the digital assets such has CrptoToken, Data Spike or hardware patch"""
+"""This class represents the digital assets such has CryptoToken, Data Spike or hardware patch"""
 class Asset:
     def __init__(self,name,description):
-        self.name = name
-        self.description = description
-        self.encrypted = False
+        self.__name = name
+        self.__description = description
+        self.__encrypted = False
 
-"""getters and setters defined """
+    """getters and setters defined """
     def get_name(self):
-        return self.name
+        return self.__name
     def get_description(self):
-        return self.description
+        return self.__description
     def get_encrypted(self):
-        return self.encrypted
-    def set_name(self,value):
-        self.name = value
-    def set_description(self,value):
-        self.description = value
-    def set_encrypted(self,value):
-        self.encrypted = bool(value)
+        return self.__encrypted
+    def set_name(self, value):
+        self.__name = value
+    def set_description(self, value):
+        self.__description = value
+    def set_encrypted(self, value):
+        self.__encrypted = bool(value)
 
+    """Using property attribute to make use of getter and setter in the other classes"""
+    name = property(get_name,set_name)
+    description = property(get_description,set_description)
+    encrypted = property(get_encrypted,set_encrypted)
+
+    def switch_encryption(self):
+        """switches the encryption from true to false"""
+        self.__encrypted = not self.__encrypted
+
+    def __str__(self):
+        if self.__encrypted:
+            return "{self.__name}: {self.__description} [Encrypted]"
+        else:
+            return "{self.__name}: {self.__description}"
 
 
 
