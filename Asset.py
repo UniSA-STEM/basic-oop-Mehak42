5e57011f128d0ee4dfca905f69f088a9810f8a39
Mehak42 <tiwmy001@mymail.unisa.edu.au>
2025-10-11 16:29:13 +1030

    Started with Asset class first - initializers and getter and setters

--- Asset.py
+++ Asset.py
@@ -1,8 +1,33 @@
 """
 File: Asset.py
-Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Description: Represents the digital assets used by Hackers and Rigs
+Author: Mehakdeep Kaur Tiwana
+ID: 110397073
+Username: TIWMY001
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+"""This class represents the digital assets such has CrptoToken, Data Spike or hardware patch"""
+class Asset:
+    def __init__(self,name,description):
+        self.name = name
+        self.description = description
+        self.encrypted = False
+
+    def get_name(self):
+        return self.name
+    def get_description(self):
+        return self.description
+    def get_encrypted(self):
+        return self.encrypted
+    def set_name(self,value):
+        self.name = value
+    def set_description(self,value):
+        self.description = value
+    def set_encrypted(self,value):
+        self.encrypted = bool(value)
+
+
+
+
+
