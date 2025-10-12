61ed7f6471fc005fb3cbb969a7751e524ea022b8
Mehak42 <tiwmy001@mymail.unisa.edu.au>
2025-10-12 16:05:34 +1030

    Added property attribute, Started with take_hits and store_assets functions in Rig class

--- Rig.py
+++ Rig.py
@@ -7,6 +7,7 @@ Username: TIWMY001
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
 
+import random
 from Asset import Asset
 
 class Rig:
@@ -41,4 +42,31 @@ class Rig:
         self.__broken = bool(value)
     def set_storage(self, value):
         self.__storage = value
+    def set_upgrade_level(self, value):
+        self.__upgrade_level = value
 
+    name = property(get_name, set_name)
+    damage = property(get_damage, set_damage)
+    broken = property(get_broken, set_broken)
+    upgrade_level = property(get_upgrade_level, set_upgrade_level)
+    storage = property(get_storage, set_storage)
+
+    def repair(self):
+        pass
+
+    def upgrade(self):
+        pass
+
+    def take_hits(self):
+        self.__damage += 1
+        if self.__damage >= 2:
+            self.__broken = True
+
+    def generate_assets(self):
+        for asset in self.__storage: pass
+
+    def store_assets(self, asset):
+        self.__storage.append(asset)
+
+    def release_asset(self, asset_name):
+        self.__storage.remove(asset_name)
