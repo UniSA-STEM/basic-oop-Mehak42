eb82764151e245d4633af020b040e294d5c304a4
Mehak42 <tiwmy001@mymail.unisa.edu.au>
2025-10-17 21:33:17 +1030

    Created two classes in Hacker.py - acquire_rig and launch_data_spike

--- Hacker.py
+++ Hacker.py
@@ -16,6 +16,7 @@ class Hacker:
         self.__inventory = []
         self.__rig = None
         self.__trace_level = 0
+        self.__exposed = False
 
         self.__inventory.append(Asset("CryptoToken", "Used to acquire or repair rigs"))
 
@@ -31,4 +32,37 @@ class Hacker:
         self.__name = name
     def set_trace_level(self, trace_level):
         self.__trace_level = max(0, int(trace_level))
+
+    name = property(get_name, set_name)
+    inventory = property(get_inventory, None)
+    rig = property(get_rig, None)
+    trace_level = property(get_trace_level, set_trace_level)
+
+    def acquire_rig(self):
+        for asset in self.__inventory:
+            if asset.name == "CryptoToken":
+                self.__inventory.remove(asset)
+                self.__rig = Rig("BeginnerRig")
+                print(f"{self.__name} acquired the BeginnerRig")
+                return
+        print("The Rig was not acquired: No CryptoToken available")
+
+    def launch_data_spikes(self, target_rig):
+        if self.__rig is None:
+            print("No rig is available to attack from")
+            return
+
+        for asset in self.__rig.storage:
+            if asset.name == "DataSpike" and not asset.encrypted:
+                self.__rig.storage.remove(asset)
+                target_rig.take_hits()
+                self.__trace_level += 1
+                print(f"{self.__name} launched a Data Spike. Trace level: {self.__trace_level}")
+                if self.__trace_level >= 5:
+                    self.__exposed = True
+                    print(f"{self.__name} exposed due to high trace level")
+                return
+
+        print(f"No data spike found for {self.__name}")
+
 
